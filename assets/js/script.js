c1aef6db95d8b78ad801e93ac906fe03dc0e92b4
Huzaifa Iftikhar Abbasi <huzaifaiftikhar671@gmail.com>
2024-02-05 16:57:07 +0500

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    // new fullpage('#fullpage', {
+    //     // anchors: ['home', 'who-are-we', 'services', 'works', 'contact', 'tos'],
+    //     // menu: '#menu-dots',
+    //     slidesNavigation: true,
+    //     autoScrolling: true,
+    //     // scrollHorizontally: true,
+    //     // scrollHorizontallyKey: 'YWx2YXJvdHJpZ28uY29tX01mU2MyTnliMnhzU0c5eWFYcHZiblJoYkd4NVNRcg==',
+    //     // afterSlideLoad: function (section, origin, destination, direction) {
+    //     //     console.log({
+    //     //         section: section,
+    //     //         origin: origin,
+    //     //         destination: destination,
+    //     //         direction: direction
+    //     //     });
+    //     // },
+    //     // onSlideLeave: function (section, origin, destination, direction) {
+    //     //     console.log({
+    //     //         section: section,
+    //     //         origin: origin,
+    //     //         destination: destination,
+    //     //         direction: direction
+    //     //     });
+    //     // }
+    // })
+});
